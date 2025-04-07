f4066d6fec22fab0dd19a2245f89c77830e19368
dawba <brian.malehorn@opendoor.com>
2025-04-08 01:06:47 +0200

    Basic db initialization

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"log"
+	"stride-wars-app/internal/repository"
 )
 
 func main() {
-	run()
 	log.Println("Running server...")
-}
 
-func run() {
-
+	client, err := repository.InitEnt()
+	if err != nil {
+		log.Fatalf("Error during Ent initialization: %v", err)
+	}
+	defer client.Close()
 }
