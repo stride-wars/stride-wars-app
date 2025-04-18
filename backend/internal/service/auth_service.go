83f937511fe615055661f2e19a924a9117c55e49
dawba <dawid.bania@opendoor.com>
2025-04-18 20:19:22 +0200

    Add basic auth service

---
+++ backend/internal/service/auth_service.go
@@ -0,0 +1,35 @@
+package service
+
+import (
+	"context"
+	"github.com/supabase-community/gotrue-go/types"
+	"github.com/supabase-community/supabase-go"
+	"go.uber.org/zap"
+)
+
+type AuthService struct {
+	supabaseClient *supabase.Client
+	logger         *zap.Logger
+}
+
+func NewAuthService(supabaseClient *supabase.Client, logger *zap.Logger) *AuthService {
+	return &AuthService{
+		supabaseClient: supabaseClient,
+		logger:         logger,
+	}
+}
+
+func (a *AuthService) SignUp(ctx context.Context, req types.SignupRequest) (interface{}, error) {
+	response, err := a.supabaseClient.Auth.Signup(req)
+	if err != nil {
+		return nil, err
+	}
+
+}
+
+func (a *AuthService) SignIn(ctx context.Context, email string, password string) (*types.TokenResponse, error) {
+	response, err := a.supabaseClient.Auth.SignInWithEmailPassword(email, password)
+	if err != nil {
+		return nil, err
+	}
+}
--- backend/internal/service/service.go
+++ backend/internal/service/service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"github.com/supabase-community/supabase-go"
 	"go.uber.org/zap"
 	"stride-wars-app/ent"
 	"stride-wars-app/internal/repository"
@@ -8,10 +9,13 @@ import (
 
 type Service struct {
 	UserService *UserService
+	AuthService *AuthService
 }
 
-func Provide(client *ent.Client, logger *zap.Logger) *Service {
+func Provide(entClient *ent.Client, supabaseClient *supabase.Client, logger *zap.Logger) *Service {
 	return &Service{
-		UserService: NewUserService(repository.NewUserRepository(client), logger),
+		UserService: NewUserService(repository.NewUserRepository(entClient), logger),
+		AuthService: NewAuthService(supabaseClient, logger),
 	}
 }
+s
