de2ada657089971a99102f414ad6e711c002b9b3
mikgrycz <mikolaj.grycz@student.uj.edu.pl>
2025-04-25 00:51:18 +0200

    auth service test

--- backend/internal/service/auth_service.go
+++ backend/internal/service/auth_service.go
@@ -3,6 +3,7 @@ package service
 import (
 	"context"
 
+	
 	"github.com/supabase-community/gotrue-go/types"
 	"github.com/supabase-community/supabase-go"
 	"go.uber.org/zap"
@@ -21,19 +22,19 @@ func NewAuthService(supabaseClient *supabase.Client, logger *zap.Logger) *AuthSe
 }
 
 func (a *AuthService) SignUp(ctx context.Context, req types.SignupRequest) (interface{}, error) {
-	_, err := a.supabaseClient.Auth.Signup(req)
+	resp, err := a.supabaseClient.Auth.Signup(req)
 	if err != nil {
+		a.logger.Error("Failed to sign up", zap.Error(err))
 		return nil, err
 	}
-
-	panic("implement me")
+	return resp, nil
 }
 
 func (a *AuthService) SignIn(ctx context.Context, email string, password string) (*types.TokenResponse, error) {
-	_, err := a.supabaseClient.Auth.SignInWithEmailPassword(email, password)
+	resp, err := a.supabaseClient.Auth.SignInWithEmailPassword(email, password)
 	if err != nil {
+		a.logger.Error("Failed to sign in", zap.Error(err))
 		return nil, err
 	}
-
-	panic("implement me")
+	return resp, nil
 }
