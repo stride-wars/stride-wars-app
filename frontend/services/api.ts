6d0823185aae35c1337c7379527c96e8b153e96a
mikgrycz <mikolaj.grycz@student.uj.edu.pl>
2025-05-17 15:42:40 +0200

    added h3

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,18 +1,22 @@
+import { h3IndexToSplitLong,  latLngToCell} from "h3-js";
+
 interface LocationData {
   latitude: number;
   longitude: number;
   timestamp: number;
 }
 
+const res = 10;
+
 export const sendLocationData = async (location: LocationData): Promise<void> => {
   try {
     // TODO: zrobic komunikacje z backendem
-    const response = await fetch('JAKIS_ENDPOINT', {
+    const response = await fetch('/api/data', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(location),
+      body: JSON.stringify(latLngToCell(location.latitude, location.longitude, res)),
     });
 
     if (!response.ok) {
